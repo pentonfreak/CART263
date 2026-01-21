3680d51cbbde0c220795745b39b1e2303c9e13bd
pentonfreak <antonmcmilan@gmail.com>
2026-01-20 20:43:43 -0500

    Q5 done

--- TASK 1/Q5/js/Q5.js
+++ TASK 1/Q5/js/Q5.js
@@ -1,70 +1,68 @@
+let counter = 0;
+let radius = 40; // base size for thef irst ellipse
+let ellipseAlpha = 20; // starting alpha for the first ellipse
+
 const button1 = {
+    w: 100,
+    h: 100,
     x: 50,
     y: 50,
-    state: "still",
+    color: "#FF0000",
+    state: "still"
 }
-
-const button2 = {
-    x: 150,
-    y: 50,
-    state: "still",
-}
-
-const counter = 0;
 
 const circle = {
     x: 300,
     y: 300,
-    alpha: 100,
+    r: 100,
 }
-let r = 50;
 
 function setup() {
     createCanvas(600, 600);
-}
+};
 
 function draw() {
     background("#000000");
 
     drawButton1();
-    drawButton2();
 
     changeColorOnHover();
 
-    drawCircle();
+    // Do not draw anything if counter is out of range
+    if (counter < 1 || counter > 10) {
+        return;
+    }
 
-   while (r >= 50) {
-    ellipse(circle.x, circle.y, r);
-    r = r + 50;
+    // Single while loop drawing `counter` ellipses centered on screen
+    let i = 0;
+    let currentRadius = radius;
+    let currentAlpha = ellipseAlpha;
+    noStroke();
+
+    while (i < counter) {
+        fill(255, currentAlpha);
+        ellipse(width / 2, height / 2, currentRadius, currentRadius);
+
+        // each next circle slightly larger and more opaque
+        currentRadius += 20;
+        currentAlpha += 20;
+
+        i++;
+    }
+}
+
+// Whenever the mouse is clicked inside the orange square increment the counter variable by 1
+function mousePressed() {
+    if (mouseX > button1.x && mouseX < button1.x + 50 && mouseY > button1.y && mouseY < button1.y + 50) {
+        counter++;
+        console.log(counter);
     }
 }
 
 function drawButton1() {
-    if ( mouseIsPressed) {
-        button1.state === "Pressed";
-        counter + 50;
-    }
-
-    push();
-    noStroke();
-    fill("#ff5900ff");
+    fill(button1.color);
     rect(button1.x, button1.y, 50, 50);
-    pop();
 }
-
-function drawButton2() {
-    if (mouseIsPressed) {
-        button2.state === "Pressed";
-        counter - 50;
-    }
-
-    push();
-    noStroke();
-    fill("#ff0000");
-    rect(button2.x, button2.y, 50, 50);
-    pop();
-}
-
 
 function changeColorOnHover() {
     if (mouseX > button1.x && mouseX < button1.x + 50 && mouseY > button1.y && mouseY < button1.y + 50) {
@@ -73,37 +71,6 @@ function changeColorOnHover() {
         rect(button1.x, button1.y, 50, 50);
     } else {
         button1.state = "still";
-    }
-
-    if (mouseX > button2.x && mouseX < button2.x + 50 && mouseY > button2.y && mouseY < button2.y + 50) {
-        button2.state = "hovered";
-        fill("#ffa200");
-        rect(button2.x, button2.y, 50, 50);
-    } else {
-        button2.state = "still";
-    }
-}
-
-// Whenever the mouse is clicked inside the orange square increment the counter variable by 1
-
-// function mousePressed() {
-//     if (mouseX > button1.x && mouseX < button1.x + 50 && mouseY > button1.y && mouseY < button1.y + 50) {
-//         counter += 50;
-//         console.log(counter);
-//     }
-//     if (mouseX > button2.x && mouseX < button2.x + 50 && mouseY > button2.y && mouseY < button2.y + 50) {
-//         counter -= 50;
-//         console.log(counter);
-//     }
-// };
-
-function drawCircle() {
-    if (mouseIsPressed) {
-    push();
-    noStroke();
-    fill(255, 255, 255, circle.alpha);
-    ellipse(circle.x, circle.y, circle.r);
-    pop();
     }
 }
 
