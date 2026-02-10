d480b10e3cc1dff3dd4464c286c70c5d3e5d7a9c
pentonfreak <antonmcmilan@gmail.com>
2026-02-09 20:51:43 -0500

    Update main.js

--- TASK 3/PROTO/js/main.js
+++ TASK 3/PROTO/js/main.js
@@ -4,22 +4,22 @@ const grid = document.getElementById("grid");
  * Pattern options:
  */
 
-// const patterns = ["+", "-", "*", "•", "×", "o"];
+const patterns = ["+", "-", "*", "•", "×", "o"];
 
 // const patterns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // const patterns = ["\u25A0", "\u25A1", "\u25A2", "\u25A3", "\u25A4", "\u25A5"];
 
-const patterns = ["0", "1"]
+// const patterns = ["0", "1"]
 
 
 /**
  * Color options:
  */
 
-// let sampleColors = ["#FF6B6B", "#6BCB77", "#4D96FF", "#FFD93D", "#FF6F91", "#845EC2"];
+let sampleColors = ["#FF6B6B", "#6BCB77", "#4D96FF", "#FFD93D", "#FF6F91", "#845EC2"];
 
-let sampleColors = ["#02ff17", "#00b418"];
+// let sampleColors = ["#02ff17", "#00b418"];
 
 /**
  * Create grid
