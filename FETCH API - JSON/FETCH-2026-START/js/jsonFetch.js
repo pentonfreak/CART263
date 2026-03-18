9417aa05a6b6d6028e21bf50a379bb7f826c52ab
Khuong Nguyen <antonmcmilan@gmail.com>
2026-03-18 18:42:37 -0400

    .

--- FETCH API - JSON/FETCH-2026-START/js/jsonFetch.js
+++ FETCH API - JSON/FETCH-2026-START/js/jsonFetch.js
@@ -1,6 +1,39 @@
-window.onload = goApp;
+window.onload = goFetch;
 
-async function goApp(){
-console.log("hello fetch")
+// Promise based fetch
+  async function goFetch(){
+  try {
+        let response = await fetch('files/tests.json');
+        console.log(response);
+        let data = await response.json();
+        console.log(data);
+        displayResults(data);
+  }
+  catch(err) { 
+      console.log(err)
+  }
+
+  function displayResults(parsedResultJS){
+    for (let i=0; i< parsedResultJS.length; i++){
+        console.log(parsedResultJS)
+        
+        //the object
+        let donutObj = parsedResultJS[i];
+        //container
+        let containerDiv = document.createElement("div");
+        containerDiv.classList.add("donutItem");
+        document.querySelector("#output").appendChild(containerDiv);
+     
+        let title = document.createElement("h3");
+        title.textContent = donutObj.name;
+        containerDiv.appendChild(title)
+
+        //access the image
+        let donutImage = document.createElement("img");
+        donutImage.src = donutObj.image;
+        containerDiv.appendChild(donutImage)
+     }
+
+}
 
 }
