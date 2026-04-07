a960fcd36e63d4c320e7025b3122ccd8e5abc9fc
pentonfreak <antonmcmilan@gmail.com>
2026-04-06 22:21:12 -0400

    .

--- TASK 7/PROTO/js/TeamD.js
+++ TASK 7/PROTO/js/TeamD.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Planet class for Team D
 export class PlanetD {
@@ -34,13 +35,13 @@ export class PlanetD {
         const moonPivot1 = new THREE.Group();
         
         const moon1 = new THREE.Mesh(
-            new THREE.SphereGeometry(0.5, 16, 16),
+            new THREE.SphereGeometry(0.4, 16, 16),
             new THREE.MeshStandardMaterial({ color: 0x888888 }) //Gray moon
         );
 
         moon1.castShadow = true;
         moon1.receiveShadow = true;
-        
+
         moon1.position.set(2, 0, 0); // Position moon 1 at a distance from the planet
         moonPivot1.add(moon1);
 
@@ -48,10 +49,50 @@ export class PlanetD {
         this.moons.push({ pivot: moonPivot1, speed: 0.5 });
 
 
+        const moonPivot2 = new THREE.Group();
+        
+        const moon2 = new THREE.Mesh(
+            new THREE.SphereGeometry(0.3, 16, 16),
+            new THREE.MeshStandardMaterial({ color: 0xaaaaaa }) //Lighter gray moon
+        );
+
+        moon2.castShadow = true;
+        moon2.receiveShadow = true;
+
+        moon2.position.x = 5; // Position moon 2 at a distance from the planet
+        moonPivot2.add(moon2);
+
+        moonPivot2.rotation.z = Math.PI / 4;
+        moonPivot2.rotation.y = Math.random() * Math.PI * 2;
+
+        this.group.add(moonPivot2);
+        this.moons.push({ pivot: moonPivot2, speed: 0.3 });
+
+
         //STEP 3:
         //TODO: Load Blender models to populate the planet with multiple props and critters by adding them to the planet group.
         //TODO: Make sure to rotate the models so they are oriented correctly relative to the surface of the planet.
-        
+
+        this.loader = new GLTFLoader();
+        this.models = [];
+
+        const modelCount = 4
+        const planetRadius = 1.5;
+
+        for (let i = 0; i < modelCount; i++) {
+            this.loader.load('models/task7.glb', (gltf) => {
+                const model = gltf.scene;
+                model.scale.set(0.5, 0.5, 0.5); // Scale down the model
+                model.castShadow = true;
+                model.receiveShadow = true;
+
+                this.group.add(model);
+                this.models.push(model);
+            });
+
+            this.scene .add(this.group);
+        }
+
         //STEP 4:
         //TODO: Use raycasting in the click() method below to detect clicks on the models, and make an animation happen when a model is clicked.
         //TODO: Use your imagination and creativity!
